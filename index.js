af7140b8c75899fccd13ea577e7bc0880101e07e
Thenura Kulathunga <thenurakulathunga@gmail.com>
2025-06-01 07:51:30 +0530

    fetch all custom properties data

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
+    const url = 'https://api.hubapi.com/crm/v3/objects/vehicles?properties=name,model,type,fuel_type';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(url, { headers });
+        const data = resp.data.results;
+
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
