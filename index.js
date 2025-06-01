f39c94ae4f48a6f1fe8d1fdaf1694149526fa8b3
Thenura Kulathunga <thenurakulathunga@gmail.com>
2025-06-01 07:50:11 +0530

    create home page route

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
